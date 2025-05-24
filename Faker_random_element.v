9dc320601cf17dcddabf7711beb383f1e88855a3
alfu32 <ioan.alferaru@gmail.com>
2025-05-24 08:51:48 +0200

    fix(random_element):khalyomede#5 fix random element

--- Faker_random_element.v
+++ Faker_random_element.v
@@ -3,12 +3,12 @@ module faker
 import rand
 
 pub fn (this Faker) random_element[T](elements []T) T {
-    number_of_elements := elements.len
-    random_index := rand.int_in_range(0, number_of_elements - 1) or {
-        panic("Failed to generate random index.")
-    }
+	number_of_elements := elements.len
+	random_index := rand.int_in_range(0, number_of_elements) or {
+		panic('Failed to generate random index.')
+	}
 
-    return elements[random_index] or {
-        panic("Failed to get random element at index ${random_index} with length ${number_of_elements}.")
-    }
+	return elements[random_index] or {
+		panic('Failed to get random element at index ${random_index} with length ${number_of_elements}.')
+	}
 }
